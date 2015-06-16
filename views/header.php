47be95bf11f42baba90f23540868b04124b2c4e4
GooBall <callumjwilliams@gmail.com>
2015-06-16 17:06:14 +0100

    Views now reuse rendering code and data logic now is independent

--- views/header.php
+++ views/header.php
@@ -5,4 +5,69 @@ require("inc/config-call.inc.php");
 require("inc/db-open.inc.php"); // MySQL Data sources
 require("inc/db-magdb-open.inc.php"); // Postgres Data Sources
 require("inc/main-nagios.inc.php"); // Nagios library
-?>
+
+function display($results) {
+
+    foreach ($results as $group_name => $group) {
+        echo "<div class='node-group' id='$group_name'>";
+        if ($group_name == true) {
+            echo "<h2 class='group-name'>$group_name</h2>";
+        }
+
+        foreach ($group as $panel_name => $panel) {
+            echo "<div class='node-panel' id='$panel_name'>";
+            echo "<h3 class='panel-name'>$panel_name</h3>";
+            foreach ($panel as $cluster_name => $cluster) {
+                echo "<div class='node-cluster' id='$cluster_name'>";
+                if ($cluster_name == true) {
+                    echo "<h5>$cluster_name</h5>";
+                };
+
+                if (!empty($cluster)) {
+                    foreach ($cluster as $node_name => $node) {
+                        // Node information
+                        $nodeInfo = "<h4>$node_name</h4>";
+                        $nodeStatus = "";
+
+                        $nodeStatus = "unknown";
+                        if ($node['status']['state'] == true) {
+                            $nodeStatus = $node['status']['state'];
+                        }
+                        if ($node['note'] == true) {
+                            $nodeNote = $node['note'];
+                            $nodeStatus .= ' note';
+                            $nodeInfo .= "<p><b>Note:</b> ".$node['note']."</p>";
+                        }
+                        $short = explode(".", $node_name);
+                        $short = $short[0];
+                        $ntup = nagios_state($short, $node_name, $nodeStatus);
+                        if ($ntup[1] == true) {
+                            $nodeStatus = $ntup[0];
+                            $nodeInfo .= '<p><b>Nagios:</b>'.$ntup[1].'</p>';
+                        }
+                        unset($ntup);
+
+                        foreach($node as $key => $value) {
+                            if (is_array($value)) {
+                                foreach($value as $k => $v) {
+                                    if ( $k != 'panel' and $k != 'cluster') {
+                                        $nodeInfo .= "<p><b>$k</b>: $v</p>\n";
+                                    }
+                                }
+                            }
+                        }
+
+                        // Renders node
+                        echo '<span id="n_'.$node_name.'" onclick="node('.$node_name.')" class="node '.$nodeStatus.'" title="'.htmlentities($nodeInfo).'"></span>';
+                    }
+                } else {
+                    // Renders no node
+                    echo "<span title='No managed systems'>&nbsp;&#x2205;</span>";
+                }
+                echo "</div>";
+            }
+            echo "</div>";
+        }
+        echo "</div>";
+    }
+}
--- views/view-logical-workers.php
+++ views/view-logical-workers.php
@@ -1,6 +1,9 @@
 <?php
 require("header.php"); // Important includes
 
+// Configuration
+$AQUILON_URL = $CONFIG['URL']['AQUILON'];
+
 // Gathers clusters
 $all_clusters = Array();
 $data = pg_query('select "name", "categoryName" from "vBatchAndCloudVMs"');
@@ -31,55 +34,36 @@ if ($status and mysql_num_rows($status)) {
     }
 }
 
+// Gathers VM node if a worker
+$personality = Array();
+$jsondata = file_get_contents("$AQUILON_URL/cgi-bin/report/host_personality_branch_nubes");
+$vms = json_decode($jsondata, true);
+foreach ($vms as $name => $values) {
+    foreach ($values as $key => $value) {
+        if (strpos($value, "workernode") !== false) {
+            $personality[$name][$value] = $name;
+        }
+    }
+}
+
 // Generates main array
 $results = Array();
-foreach ($all_clusters as $name => $cluster) {
-    $results[$cluster][$name] = Array();
-    if (array_key_exists($name, $all_notes)) {
-        $results[$cluster][$name]['note'] = $all_notes[$name];
-    };
-    if (array_key_exists($name, $all_status)) {
-        $results[$cluster][$name]['status'] = $all_status[$name];
-    };
+foreach ($all_clusters as $name => $panels) {
+    if (($panels != "vm-nubes") or (array_key_exists($name, $personality) == true)) {
+
+        $group = '';
+        $panel = $panels;
+        $cluster = '';
+
+        $results[$group][$panel][$cluster][$name] = Array();
+        if (array_key_exists($name, $all_notes)) {
+            $results[$group][$panel][$cluster][$name]['note'] = $all_notes[$name];
+        };
+        if (array_key_exists($name, $all_status)) {
+            $results[$group][$panel][$cluster][$name]['status'] = $all_status[$name];
+        };
+    }
 }
 
 // Renders page
-echo "<div class=\"cluster-container\">";
-foreach ($results as $cluster_name => $cluster) {
-    echo "<div class=\"cluster\"><h5>$cluster_name</h5>";
-    foreach ($cluster as $node_name => $node) {
-
-        // Node information
-        $nodeInfo = "<h4>$node_name</h4>";
-
-        if ($node['status']['source'] == true) {
-            $nodeInfo .= "<p><b>Source:</b> ".$node['status']['source']."</p>";
-        }
-
-        if ($node['status']['state'] == true) {
-            $nodeStatus = $node['status']['state'];
-            $nodeInfo .= "<p><b>Status:</b> ".$nodeStatus."</p>";
-        } else {
-            $nodeStatus = "unknown";
-        }
-
-        if ($node['note'] == true) {
-            $nodeNote = $node['note'];
-            $nodeStatus .= ' note';
-            $nodeInfo .= "<p><b>Note:</b> ".$node['note']."</p>";
-        }
-        $short = explode(".", $node_name);
-        $short = $short[0];
-        $ntup = nagios_state($short, $node_name, $nodeStatus);
-        if ($ntup[1] == true) {
-            $nodeStatus = $ntup[0];
-            $nodeInfo .= '<p><b>Nagios:</b>'.$ntup[1].'</p>';
-        }
-        unset($ntup);
-
-        // Renders node
-        echo '<span id="n_'.$node_name.'" onclick="node(\''.$node_name.'\')" class="node '.$nodeStatus.'" title="'.htmlentities($nodeInfo).'"></span>';
-    }
-    echo "</div>\n";
-}
-echo "</div>\n";
+display($results);
