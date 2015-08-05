67cf7833d6d116351fd3f2bc12397c8a30818517
James Adams <james.adams@stfc.ac.uk>
2015-08-05 16:45:06 +0100

    Move renderinf of overwatch info into a seperate function

--- node/node-magdb.inc.php
+++ node/node-magdb.inc.php
@@ -152,6 +152,67 @@ class pMagdb
         }
     }
 
+    private function render_overwatch_info($overwatch_info)
+    {
+        echo "<h3>Overwatch</h3>\n";
+        echo "<dl>\n";
+        foreach ($overwatch_info as $col => $val) {
+            if ($val === null) {
+                $val = "&nbsp;";
+            }
+            //Find RT ticket number and linkify them
+            if ($col == "miscComments") {
+                $val = preg_replace("/#\s*([0-9][0-9]*)/", '<a href="'.$HELPDESK_URL.'/Ticket/Display.html?id=$1">#$1</a>', $val);
+                $val = preg_replace("/RT\s*([0-9][0-9]*)/", '<a href="'.$HELPDESK_URL.'/Ticket/Display.html?id=$1">RT$1</a>', $val);
+                $val = "<span>$val</span>";
+            } elseif ($col == "machineName") {
+                $val = "<a href=\"$OVERWATCH_URL/index.php?update:$val\" title=\"Update Overwatch record\">$val</a>\n";
+            } elseif ($col == "normalStatus") {
+                $val = "<a href=\"$OVERWATCH_URL/index.php?view:status:$val\" title=\"Overwatch servers with status $val\">$val</a>\n";
+            } elseif ($col == "diskPool") {
+                $val = "<a href=\"$OVERWATCH_URL/index.php?view:diskpool:$val\" title=\"Overwatch servers in $val diskpool\">$val</a>\n";
+            } elseif ($col == "castorInstance") {
+                $val = "<a href=\"$OVERWATCH_URL/index.php?view:instance:$val\" title=\"Overwatch servers in $val instance\">$val</a>\n";
+            }
+            echo "<dt>$col</dt><dd>$val</dd>\n";
+        }
+        echo "</dl>\n";
+
+        $history = $this->getOverwatchHistory($NODE);
+        if ($history) {
+            echo "<h3><span class=\"rollup\" onclick=\"toggleRollup('#node-magdb-history');\" title=\"Rollup Section\">&#x25BE; Overwatch State History</span></h3>";
+            echo "<div id=\"node-magdb-history\"";
+            if (isset($_COOKIE["rollup_#node-magdb-history"]) and $_COOKIE["rollup_#node-magdb-history"] == "hidden") {
+                echo " style=\"display: none\"";
+            }
+            echo ">\n";
+            echo "<table class=\"timeline\">";
+            echo "<tr>";
+            foreach (Array("When","Who","Current","Normal","Team","Service Type","VO","Pool","Size","P","Q","Comments") as $h) {
+                echo "<th>$h</th>";
+            }
+            echo "</tr>";
+            $previous_row = array_merge(Array("lastUpdatedBy" => "Nobody", "lastUpdateDate" => "Current"), $overwatch_info); # Previous row
+
+            //Copy relevant parts of current state to history for comparison
+            $fr = Array();
+            foreach (array_keys($history[0]) as $column) {
+                $fr[$column] = $previous_row[$column];
+            }
+            $history[] = $fr;
+
+            $previous_row = False;
+
+            //Display history
+            render_history_rows();
+            echo "</table>";
+            echo "</div>\n";
+        }
+        else {
+            echo "<p class=\"info\">No Overwatch History Found</p>";
+        }
+    }
+
     private function render_pdu_list($room_pdus)
     {
         echo "<dl>\n";
@@ -229,63 +290,7 @@ class pMagdb
 
         $overwatch_info = $this->getDBinfo($SHORT);
         if ($overwatch_info !== null) {
-            echo "<h3>Overwatch</h3>\n";
-            echo "<dl>\n";
-            foreach ($overwatch_info as $col => $val) {
-                if ($val === null) {
-                    $val = "&nbsp;";
-                }
-                //Find RT ticket number and linkify them
-                if ($col == "miscComments") {
-                    $val = preg_replace("/#\s*([0-9][0-9]*)/", '<a href="'.$HELPDESK_URL.'/Ticket/Display.html?id=$1">#$1</a>', $val);
-                    $val = preg_replace("/RT\s*([0-9][0-9]*)/", '<a href="'.$HELPDESK_URL.'/Ticket/Display.html?id=$1">RT$1</a>', $val);
-                    $val = "<span>$val</span>";
-                } elseif ($col == "machineName") {
-                    $val = "<a href=\"$OVERWATCH_URL/index.php?update:$val\" title=\"Update Overwatch record\">$val</a>\n";
-                } elseif ($col == "normalStatus") {
-                    $val = "<a href=\"$OVERWATCH_URL/index.php?view:status:$val\" title=\"Overwatch servers with status $val\">$val</a>\n";
-                } elseif ($col == "diskPool") {
-                    $val = "<a href=\"$OVERWATCH_URL/index.php?view:diskpool:$val\" title=\"Overwatch servers in $val diskpool\">$val</a>\n";
-                } elseif ($col == "castorInstance") {
-                    $val = "<a href=\"$OVERWATCH_URL/index.php?view:instance:$val\" title=\"Overwatch servers in $val instance\">$val</a>\n";
-                }
-                echo "<dt>$col</dt><dd>$val</dd>\n";
-            }
-            echo "</dl>\n";
-
-            $history = $this->getOverwatchHistory($NODE);
-            if ($history) {
-                echo "<h3><span class=\"rollup\" onclick=\"toggleRollup('#node-magdb-history');\" title=\"Rollup Section\">&#x25BE; Overwatch State History</span></h3>";
-                echo "<div id=\"node-magdb-history\"";
-                if (isset($_COOKIE["rollup_#node-magdb-history"]) and $_COOKIE["rollup_#node-magdb-history"] == "hidden") {
-                    echo " style=\"display: none\"";
-                }
-                echo ">\n";
-                echo "<table class=\"timeline\">";
-                echo "<tr>";
-                foreach (Array("When","Who","Current","Normal","Team","Service Type","VO","Pool","Size","P","Q","Comments") as $h) {
-                    echo "<th>$h</th>";
-                }
-                echo "</tr>";
-                $previous_row = array_merge(Array("lastUpdatedBy" => "Nobody", "lastUpdateDate" => "Current"), $overwatch_info); # Previous row
-
-                //Copy relevant parts of current state to history for comparison
-                $fr = Array();
-                foreach (array_keys($history[0]) as $column) {
-                    $fr[$column] = $previous_row[$column];
-                }
-                $history[] = $fr;
-
-                $previous_row = False;
-
-                //Display history
-                render_history_rows();
-                echo "</table>";
-                echo "</div>\n";
-            }
-            else {
-                echo "<p class=\"info\">No Overwatch History Found</p>";
-            }
+            render_overwatch_info($overwatch_info);
         }
     }
 }
