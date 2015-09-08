3e3e06427a5033c32eb6cae02e39ffee05a429a6
GooBall <callum.williams@stfc.ac.uk>
2015-09-08 11:29:14 +0100

    Even more cleaning and removal of superglobals

--- node/node-getName.inc.php
+++ node/node-getName.inc.php
@@ -1,8 +1,11 @@
 <?php
 // Check the node
-$NODE = (isset($_REQUEST["n"])) ? $_REQUEST["n"] : "[unknown]";
+$NODE = filter_input(INPUT_GET, 'n', FILTER_SANITIZE_STRING);
+if ($NODE === Null){
+    $NODE = "[Unknown]";
+}
 $NODE = preg_replace("/[^a-zA-Z0-9.-]/", "", $NODE); #strip out all but valid hostname characters
 $SHORT = $NODE;
 if (substr($NODE, -3, 3) == ".uk") {
-  list($SHORT,$rest) = explode(".", $NODE, 2);
+    list($SHORT,$rest) = explode(".", $NODE, 2);
 }
