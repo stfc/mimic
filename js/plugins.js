330cb894269d8057202b9472a6eab81ad5e7bb62
GooBall <callum.williams@stfc.ac.uk>
2015-09-08 11:48:33 +0100

    Fixed indentation, operators and added missing missing semicolon

--- js/plugins.js
+++ js/plugins.js
@@ -1,7 +1,7 @@
 function toggleRollup(c_name) {
     // Check whether user has consented to cookie usage
-    if (Cookies.get('cb-enabled') == "accepted") {
-        if (Cookies.get("rollup_" + c_name) == "hidden") {
+    if (Cookies.get('cb-enabled') === "accepted") {
+        if (Cookies.get("rollup_" + c_name) === "hidden") {
 		    $(c_name).show("blind");
             Cookies.get("rollup_" + c_name, "shown");
         } else {
