d96516bc2130e7ec38beef30d7c1f3f17a746f26
GooBall <callum.williams@stfc.ac.uk>
2015-09-08 11:48:33 +0100

    Fixed indentation, operators and added missing semicolon

--- js/plugins.js
+++ js/plugins.js
@@ -1,1 +1,14 @@
-function toggleRollup(a){"accepted"==Cookies.get("cb-enabled")?"hidden"==Cookies.get("rollup_"+a)?($(a).show("blind"),Cookies.get("rollup_"+a,"shown")):($(a).hide("blind"),Cookies.get("rollup_"+a,"hidden")):$(a).toggle("blind")}
+function toggleRollup(c_name) {
+    // Check whether user has consented to cookie usage
+    if (Cookies.get('cb-enabled') === "accepted") {
+        if (Cookies.get("rollup_" + c_name) === "hidden") {
+		    $(c_name).show("blind");
+            Cookies.get("rollup_" + c_name, "shown");
+        } else {
+		    $(c_name).hide("blind");
+            Cookies.get("rollup_" + c_name, "hidden");
+        }
+    } else {
+        $(c_name).toggle("blind");
+    }
+}
