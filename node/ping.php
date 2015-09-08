57c080bec6fec68a60de43c3c7ff355d342307c4
GooBall <callum.williams@stfc.ac.uk>
2015-09-08 11:29:14 +0100

    Even more cleaning and removal of superglobals

--- node/ping.php
+++ node/ping.php
@@ -3,36 +3,32 @@
   $node = filter_input(INPUT_GET, 'node', FILTER_SANITIZE_STRING);
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
+<head>
     <title>Tier1A monitor: <?php echo htmlspecialchars($node) ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="refresh" content="300" />
     <link rel="stylesheet" type="text/css" href="../css/info.css" media="screen" />
-  </head>
-  <body>
+</head>
+<body>
 <?php
     preg_match('/[A-Za-z0-9.]+/', $node, $node); //Only count hostname characters
     if (is_array($node)) {
-      $node = $node[0];
-      if ($node != "") {
-        echo "    <div class=\"head\">Ping ".htmlspecialchars(urlencode($node))."</div>\n";
-        flush();
-        ob_flush();
-        echo "    <pre>\n";
-        system('ping -c 5 '.urlencode($node));
-        echo "    </pre>\n";
-      }
-      else {
-        echo "     <p class=\"error\">Blank hostname provided</p>\n";
-      }
+        $node = $node[0];
+        if ($node != "") {
+            echo "<div class='head'>Ping ".htmlspecialchars(urlencode($node))."</div>\n";
+            flush();
+            ob_flush();
+            echo "<pre>";
+            system('ping -c 5 '.urlencode($node));
+            echo "</pre>\n";
+        }
+        else {
+            echo "<p class='error'>Blank hostname provided</p>";
+        }
     }
     else {
-      echo "     <p class=\"error\">Invalid hostname</p>\n";
+      echo "<p class='error'>Invalid hostname</p>";
     }
-  }
-  else {
-    echo "     <p class=\"error\">No hostname provided</p>\n";
-  }
 ?>
-  </body>
+</body>
 </html>
