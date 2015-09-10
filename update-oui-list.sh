0ceefa45c54f6c23d21510c4bfe1116bb174e6a5
GooBall <callum.williams@stfc.ac.uk>
2015-09-10 13:37:59 +0100

    Moved, renamed, and updated update-oui-list.sh

---
+++ update-oui-list.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo ''
+echo '#################################################################'
+echo 'Generating oui-list.php in config/ --- This may take some time...'
+echo '#################################################################'
+echo ''
+echo 'Working...'
+echo '<?php' > config/oui-list.php
+echo '$ouilist = array(' >> config/oui-list.php
+curl -s "http://standards-oui.ieee.org/oui.txt" | grep "(base 16)" |  sed 's!"!\&quot;!g' | sed 's!^\([0-9a-fA-F]\{6\}\)\s\+(base 16)\s\+\(.\+\)\r$!"\1"=>"\2",!g' | tr -d '\n' >> config/oui-list.php
+echo ');' >> config/oui-list.php
+echo ''
+echo '#################################################################'
+echo '                             Done!'
+echo '#################################################################'
