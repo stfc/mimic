d6a6b77f30cf66f68813498ba13eda1e8e1288fd
James Adams <james.adams@stfc.ac.uk>
2015-09-11 17:14:49 +0100

    Use four-space indentation
    PEP8!

--- node/node-requesttracker-rest.py
+++ node/node-requesttracker-rest.py
@@ -8,7 +8,7 @@ import json
 import ConfigParser
 
 def parse_ticket_lines(lines):
-  return dict([[w.replace("'","").replace('"','').strip() for w in l[:-1].split(':', 1)] for l in lines if ":" in l])
+    return dict([[w.replace("'","").replace('"','').strip() for w in l[:-1].split(':', 1)] for l in lines if ":" in l])
 
 # Calling ini file
 config = ConfigParser.ConfigParser()
@@ -20,54 +20,54 @@ access_user = config.get("HELPDESK", "USER")
 access_password = config.get("HELPDESK", "PASS")
 
 if len(argv) == 2 or len(argv) == 3:
-  subject = argv[1]
+    subject = argv[1]
 
-  history = False;
+    history = False;
 
-  if len(argv) == 3:
-    if argv[2] == "history":
-      history = True;
+    if len(argv) == 3:
+        if argv[2] == "history":
+            history = True;
 
-  # trying login on rt server, need a cookie jar to to store connection bits
-  cj = LWPCookieJar()
+    # trying login on rt server, need a cookie jar to to store connection bits
+    cj = LWPCookieJar()
 
-  opener = build_opener(HTTPCookieProcessor(cj))
+    opener = build_opener(HTTPCookieProcessor(cj))
 
-  install_opener(opener)
+    install_opener(opener)
 
-  credentials = {'user':access_user,'pass':access_password}
-  credentials = urlencode(credentials)
+    credentials = {'user':access_user,'pass':access_password}
+    credentials = urlencode(credentials)
 
-  login  = Request(uri, credentials)
+    login  = Request(uri, credentials)
 
-  # make query using TicketSQL
-  query = "Subject like '%s'" % subject
-  if not history:
-    query = query + " AND ( Status = 'new' OR Status = 'open' )"
-  query = quote(query)
-  search = Request(uri + "/REST/1.0/search/ticket/" + '?query=' + query)
-
-  try:
-    response = urlopen(login)
+    # make query using TicketSQL
+    query = "Subject like '%s'" % subject
+    if not history:
+        query = query + " AND ( Status = 'new' OR Status = 'open' )"
+    query = quote(query)
+    search = Request(uri + "/REST/1.0/search/ticket/" + '?query=' + query)
 
     try:
-      response = urlopen(search)
-      tickets = response.readlines()
-      details = parse_ticket_lines(tickets[2:])
+        response = urlopen(login)
 
-      for id, subject in tickets.iteritems():
-        details = urlopen(Request(uri + "/REST/1.0/ticket/" + id + "/show")).readlines()[2:-1]
-        details = parse_ticket_lines(details)
-        tickets[id] = (tickets[id], details["Queue"], details["Created"], details["Status"])
+        try:
+            response = urlopen(search)
+            tickets = response.readlines()
+            details = parse_ticket_lines(tickets[2:])
 
-      print(json.dumps(tickets))
+            for id, subject in tickets.iteritems():
+                details = urlopen(Request(uri + "/REST/1.0/ticket/" + id + "/show")).readlines()[2:-1]
+                details = parse_ticket_lines(details)
+                tickets[id] = (tickets[id], details["Queue"], details["Created"], details["Status"])
+
+            print(json.dumps(tickets))
+        except:
+            print('{}')
+            #print(exc_info()[0])
     except:
-      print('{}')
-      #print(exc_info()[0])
-  except:
-    print('{}')
-    #print(exc_info()[0])
+        print('{}')
+        #print(exc_info()[0])
 else:
-  # no search requested
-  print('{}')
-  exit(1)
+    # no search requested
+    print('{}')
+    exit(1)
