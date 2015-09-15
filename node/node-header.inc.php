3655ac3c81537e14035788d615fa69dc43e4d0d6
GooBall <callum.williams@stfc.ac.uk>
2015-09-15 11:40:38 +0100

    Missing h1 tag

--- node/node-header.inc.php
+++ node/node-header.inc.php
@@ -1,8 +1,7 @@
 <?php global $NODE; ?>
 <div class="head">
-  <h1>
-    Node <?php echo htmlspecialchars($NODE) . "\n" ?>
-    <a class="button" href="ssh://root@<?php echo $NODE ?>">SSH</a>
+    <h1>Node <?php echo htmlspecialchars($NODE) . "\n" ?></h1>
+    <a class="tab" href="ssh://root@<?php echo $NODE ?>">SSH</a>
     <?php
     //get number from hostname
     preg_match('/[0-9]+/', $NODE, $number);
@@ -20,11 +19,14 @@
     $name_parts = preg_split('/[0-9]+/', $NODE);
 
     //output next-door nodes
+    $prev_machine = $name_parts[0].sprintf("%0${number_digits}s", $number - 1).$name_parts[1];
+    $next_machine = $name_parts[0].sprintf("%0${number_digits}s", $number + 1).$name_parts[1];
+
     if ($number_digits > 0) {
         if ($number > 0) {
-            echo '<a class="tab" href="node.php?n='.$name_parts[0].sprintf("%0${number_digits}s", $number - 1).$name_parts[1]."\"><span class='glyphicon glyphicon-circle-arrow-left'></span></a>\n";
+            echo '<a class="tab" href="node.php?n='.$prev_machine.'" title="'.$prev_machine.'"><span class="glyphicon glyphicon-circle-arrow-left"></span></a>';
         }
-      echo '<a class="tab" href="node.php?n='.$name_parts[0].sprintf("%0${number_digits}s", $number + 1).$name_parts[1]."\"><span class='glyphicon glyphicon-circle-arrow-right'></span></a>\n";
+        echo '<a class="tab" href="node.php?n='.$next_machine.'" title="'.$next_machine.'"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>';
     }
     ?>
 
