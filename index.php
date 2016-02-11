f54e2161b229c2340827d1969c0ca822d553c5c7
GooBall <callum.williams@stfc.ac.uk>
2016-02-11 13:08:22 +0000

    Move rendering client side and use masonry to append content

--- index.php
+++ index.php
@@ -7,32 +7,7 @@
 </div>
 
 <script type="text/javascript">
-
-if (Cookies.get('view')) {
-    view(Cookies.get('view'));
-} else {
-    view('logical-workers');
-}
-
-function view(requested_view) {
-    $('.loading').show();   // Overlay loading svg
-    $('.tab').removeClass('active');   // Clear all tabs so none show as selected
-    Cookies.set('view', requested_view);   // Set the requested view as a cookie
-
-    current_view = Cookies.get('view');
-    $('#' + current_view).addClass('active');
-    getData(current_view);
-}
-
-function getData(current_view) {
-    $.ajax({
-        dataType: "json",
-        url: 'views/view-' + current_view + '.php',
-        cache: false
-    }).done(gotData);
-}
-
-
+// Initialise masonry
 $grid = $('#farm').masonry({
     gutter: 16,
     itemSelector: '.grid-item',
@@ -40,103 +15,93 @@ $grid = $('#farm').masonry({
     transitionDuration: 0
 });
 
+run_view();   // Get content on page load
+window.setInterval(run_view, 10000);   // Then every 60 seconds
 
+function run_view() {
+    if (Cookies.get('view')) {
+        current_view = Cookies.get('view');
+    } else {
+        current_view = 'logical-workers';   // Set default view to load if no cookie found
+    }
 
+    $('#' + current_view).addClass('active');
+    getData(current_view);
+}
 
+// Run when menu tab clicked
+function view(requested_view) {
+    $('.loading').show();   // Overlay loading svg
+    $('.tab').removeClass('active');   // Clear all tabs so none show as selected
+    Cookies.set('view', requested_view);   // Set the requested view as a cookie
+    run_view();
+}
 
-
+function getData(current_view) {
+    $.ajax({
+        dataType: 'json',
+        url: 'views/view-' + current_view + '.php',
+        cache: false
+    }).done(gotData);
+}
 
 function gotData(data) {
     $('.loading').hide();
-    var finished_node = "";
+    finished_node = '';   // Final output
 
     for (group_name in data) {
         group_data = data[group_name];
-        console.log('[group] ' + group_name);
-
-        finished_node += "<div class='node-group' id='"+group_name+"'>";
+        finished_node += '<div class="node-group" id="' + group_name + '">';
         if (group_name !== "") {
             finished_node += "<h2 class='group-name  grid-item'>"+group_name+"</h2>";
         }
+
         for (panel_name in group_data) {
             panel_data = group_data[panel_name];
-            console.log('    [panel] ' + panel_name);
-
-
-            finished_node += "<div class='node-panel grid-item' id='"+panel_name+"'>";
-            finished_node += "<h3 class='panel-name'>"+panel_name+"</h3>";
+            finished_node += '<div class="node-panel grid-item" id="' + panel_name + '">';
+            finished_node += '<h3 class="panel-name">' + panel_name + '</h3>';
 
             for (cluster_name in panel_data) {
                 cluster_data = panel_data[cluster_name];
-                console.log('        [cluster] ' + cluster_name);
-
-                if (cluster_name !== "") {
-                    finished_node += "<h5 class='cluster-name'>"+cluster_name+"</h5>";
+                if (cluster_name !== '') {
+                    finished_node += '<h5 class="cluster-name">' + cluster_name + '</h5>';
                 }
 
                 for (node_name in cluster_data) {
-                    // console.log(cluster_data.length);
-
                     node_data = cluster_data[node_name];
-                    console.log('            [node] ' + node_name);
-                    node_info = "<h4>"+node_name+"</h4>";
+                    node_info = '<h4>' + node_name + '</h4>';
 
-                    if (("status" in node_data) && ("state" in node_data.status)) {
-                        node_status = node_data.status.state;
-                        node_info += "<p><b>State:</b> "+node_status+"</p>";
+                    // Status
+                    if (('status' in node_data) && ('state' in node_data.status)) {
+                        node_info += '<p><b>State:</b> ' + node_data.status.state + '</p>';
                     } else {
-                        node_status = "unknown";
+                        node_status = 'unknown';
                     }
 
-                    if (("status" in node_data) && ("source" in node_data.status)) {
-                        node_info += "<p><b>Source:</b> "+node_data.status.source+"</p>";
+                    // Source
+                    if (('status' in node_data) && ('source' in node_data.status)) {
+                        node_info += '<p><b>Source:</b> ' + node_data.status.source + '</p>';
                     }
 
-                    if (("note" in node_data)) {
-                        node_status += " note";
-                        note = node_data.note;
-                        node_info += "<p><b>Note:</b> &quot;"+note+"&quot;</p>"
+                    // Note
+                    if (('note' in node_data)) {
+                        node_status += ' note';
+                        node_info += '<p><b>Note:</b> &quot;' + node_data.note + '&quot;</p>';
                     }
 
-                    finished_node += '<span id="'+node_name+'" class="node '+node_status+'" title="'+node_info+'"></span>';
+                    // Node
+                    finished_node += '<span id="' + node_name + '" class="node ' + node_status + '" title="' + node_info + '"></span>';
                 }
             }
-            finished_node += "</div>";
+            finished_node += '</div>';
         }
-        finished_node += "</div>";
+        finished_node += '</div>';
     }
 
     $('#farm').empty();
 
     $grid.prepend( finished_node ).masonry( 'reloadItems' );
     $grid.masonry();
-
-    $('.node').click(function() {
-        open_node(this.id)
-    });
-
-}
-
-
-
-
-
-
-
-
-
-
-var node_window = null;
-
-function open_node(node_name) {
-    node_window = window.open("node.php?n="+node_name, "node", "width=640,height=480,left=128,top=128,scrollbars=yes,toolbar=no,status=no");
-    node_window.window.focus();
-}
-
-// ------------------------------------------
-// Makes things look nice                  f3
-// ------------------------------------------
-function layout() {
 
     // Node search
     locateNode($('#inLocate').val());
@@ -149,37 +114,13 @@ function layout() {
         delay: {"show": 100}
     });
 
+    // Node window
+    $('.node').click(function() {
+        open_node(this.id);
+    });
+
     // Runs from js/key.js
     updateKey();
-
-    // Increases width of panel if number of nodes is too high
-    // $.each($('.node-cluster'), nodeCount);
 }
-
-// ------------------------------------------
-// Counts nodes and does stuff             f4
-// ------------------------------------------
-function nodeCount() {
-
-    // If a cluster has too many nodes this will apply the 'grid-item-big' class to it
-
-
-    // Adds title with number of nodes in
-    // if ($(".cluster-name")[0]) {
-    //     $(this).children(".cluster-name").prop('title', "This section contains " + $con_width + " nodes");
-    // } else {
-    //     $(this).prev().prop('title', "This section contains " + $con_width + " nodes");
-    // }
-
-}
-
-$('.tab').click(function () {
-    view(this.id);
-});
-
-// loading();
-getData();
-// window.setInterval(getData, 20000); // Every 60 seconds
-
 </script>
 <?php require_once('footer.php'); ?>
--- views/header.php
+++ views/header.php
@@ -1,124 +1,14 @@
 <?php
 require("inc/config-call.inc.php");
 require("inc/functions.inc.php");
-
-function display($results) {
-    foreach ($results as $group_name => $group) {
-        if (!empty($group_name)) {
-            echo "<h2 class='group-name'>$group_name</h2>";
-        }
-        echo "<div class='node-group' id='$group_name'>";
-        foreach ($group as $panel_name => $panel) {
-            echo "<div class='node-panel grid-item' id='$panel_name'>";
-            echo "<h3 class='panel-name'>$panel_name</h3>";
-            foreach ($panel as $cluster_name => $cluster) {
-                echo "<div class='node-cluster' id='$cluster_name'>";
-                if (!empty($cluster_name)) {
-                    echo "<h5 class='cluster-name'>$cluster_name</h5>";
-                };
-                nodes($cluster);
-                echo "</div>";
-            }
-            echo "</div>";
-        }
-        echo "</div>";
-    }
-}
-
-function nodes($cluster, $group_name) {
-    if (!empty($cluster)) {
-        foreach ($cluster as $node_name => $node) {
-            // Node information
-            $nodeInfo = "<h4>$node_name</h4>";
-            $nodeStatus = "unknown";
-            if (array_key_exists('status', $node)) {
-                if (array_key_exists('state', $node['status'])) {
-                    if (!empty($node['status']['state'])) {
-                        $nodeStatus = $node['status']['state'];
-                    }
-                }
-
-                // VMs only - Node shows as critical if conditions are met
-                if (strpos($node_name, "vm") !== false and $group_name == "vm") {
-                    if ($node['status']['state'] == "uninstantiated") {
-                        if ($node["branch_name"] != "prod") {
-                            $nodeStatus .= ' critical';
-                            $nodeInfo .= "<p><b>Aquilon:</b> Warning: Uninstantiated VM not in prod, needs to be cleaned up!</p>";
-                        }
-                        if ($node["personality"] != "nubesvms") {
-                            $nodeStatus .= ' critical';
-                            $nodeInfo .= "<p><b>Aquilon:</b> Warning: Uninstantiated VM personality not nubesvms, needs to be cleaned up!</p>";
-                        }
-                    }
-                    elseif ($node["branch_name"] != "prod") {
-                        $nodeStatus .= ' warning';
-                        $nodeInfo .= "<p><b>Aquilon:</b> Warning: VM host not in prod</p>";
-                    }
-                }
-            }
-            if (array_key_exists('note', $node)) {
-                if (!empty($node['note'])) {
-                    $nodeNote = $node['note'];
-                    $nodeStatus .= ' note';
-                    $nodeInfo .= "<p><b>Note:</b> &quot;".$node['note']."&quot;</p>";
-                }
-            }
-            $short = explode(".", $node_name);
-            $short = $short[0];
-            $ntup = nagios_state($short, $node_name, $nodeStatus);
-            if (!empty($ntup[1])) {
-                $nodeStatus = $ntup[0];
-                $nodeInfo .= '<p><b>Nagios:</b>'.$ntup[1].'</p>';
-            }
-            unset($ntup);
-
-            foreach($node as $key => $value) {
-                if (is_array($value)) {
-                    foreach($value as $k => $v) {
-                        if ( $k != 'panel' and $k != 'cluster') {
-                            $nodeInfo .= "<p><b class='info-type'>$k</b>: $v</p>\n";
-                        }
-                    }
-                }
-            }
-
-            // Renders node
-            echo '<span id="n_'.$node_name.'" onclick="node(\''.$node_name.'\')" class="node '.$nodeStatus.'" title="'.htmlentities($nodeInfo).'"></span>';
-        }
-    } else {
-        // Renders no node
-        echo "<span title='No managed systems'>&nbsp;&#x2205;</span>";
-    }
-}
-
-function generational_results($all_clusters, $all_notes) {
-    // Generates main array for view-generational-hosts.php and view-generational-routing.php
-    $results = Array();
-    foreach ($all_clusters as $name => $panels) {
-
-        $group = '';
-        $panel = $panels['panel'];
-        $cluster = '';
-
-        $results[$group][$panel][$cluster][$name] = Array();
-        if (array_key_exists($name, $all_notes)) {
-            $results[$group][$panel][$cluster][$name]['note'] = $all_notes[$name];
-        };
-        if (array_key_exists($name, $all_clusters)) {
-            $results[$group][$panel][$cluster][$name]['status'] = $all_clusters[$name];
-        };
-    }
-
-    // Renders page
-    display($results);
-}
+header('Content-Type: application/json');
 
 function bool2str($value) {
-  // PHP is pretty bad at representing booleans in a human readable way so we'll do it ourselves
-  if ($value === true) {
-      $value = "true";
-  } elseif ($value === false) {
-      $value = "false";
-  }
-  return($value);
+    // PHP is pretty bad at representing booleans in a human readable way so we'll do it ourselves
+    if ($value === true) {
+        $value = "true";
+    } elseif ($value === false) {
+        $value = "false";
+    }
+    return($value);
 }
