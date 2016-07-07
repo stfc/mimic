04c7044b4288f088cbe538039f17f911289c43a0
GooBall <callum.williams@stfc.ac.uk>
2016-07-07 13:45:27 +0100

    Read new config section from view output

--- index.php
+++ index.php
@@ -51,91 +51,101 @@ function getData(current_view) {
         gotData(d);
     });
 }
-
+var clickable;
 function gotData(data) {
     $('.loading').hide();
     finished_node = '';   // Final output
 
+    // Check to see if nodes can be clicked
+    if (typeof data.config != 'undefined') {
+        clickable = data.config['clickable'];
+    } else {
+        // Allow clicking if undefined
+        clickable = true;
+    }
+
     for (group_name in data) {
-        group_data = data[group_name];
-        finished_node += '<div class="node-group" id="' + group_name + '">';
-        if (group_name !== "") {
-            finished_node += "<h2 class='group-name  grid-item'>"+group_name+"</h2>";
-        }
-
-        for (panel_name in group_data) {
-            panel_data = group_data[panel_name];
-            new_box_size = '';
-
-            if (group_name !== "sandbox") {
-                for (cluster_name in panel_data) {
-                    length = Object.keys(panel_data[cluster_name]).length;
-                    if (length > 360) {
-                        new_box_size += ' step1';
-                    }
-                }
+        if (group_name != 'config') {
+            group_data = data[group_name];
+            finished_node += '<div class="node-group" id="' + group_name + '">';
+            if (group_name !== "") {
+                finished_node += "<h2 class='group-name  grid-item'>"+group_name+"</h2>";
             }
 
-            finished_node += '<div class="node-panel grid-item' + new_box_size + '" id="' + panel_name + '">';
-            finished_node += '<h3 class="panel-name">' + panel_name + '</h3>';
+            for (panel_name in group_data) {
+                panel_data = group_data[panel_name];
+                new_box_size = '';
 
-            for (cluster_name in panel_data) {
-                cluster_data = panel_data[cluster_name];
-
-                if (current_view === 'elasticsearch-routing') {
-                    finished_node += '<div class="rack">';
-                } else {
-                    finished_node += '<div class="node-cluster">';
-                }
-
-                if (cluster_name !== '') {
-                    finished_node += '<h5 class="cluster-name">' + cluster_name + '</h5>';
-                }
-
-                if (cluster_data.length !== 0) {
-                    for (node_name in cluster_data) {
-                        node_data = cluster_data[node_name];
-                        node_info = '<h4>' + node_name + '</h4>';
-
-                        node_status = 'unknown';
-
-                        for (info in node_data) {
-                            info_body = '';
-
-                            if (info === 'status') {
-                                info = '<h4>Status:</h4>';
-                                node_status = "";
-                                for (status in node_data['status']) {
-                                    node_status += status;
-                                }
-                                info_body += '<ul><b>' + node_status.toLowerCase() + '</b> - ' + node_data['status'][status] + '</ul>';
-                            } else {
-                                info_body = node_data[info];
-                                info = '<b>' + info.replace(/_/g, ' ') + '</b>: ';
-                            }
-                            node_info += '<p>' + info + info_body +'</p>';
+                if (group_name !== "sandbox") {
+                    for (cluster_name in panel_data) {
+                        length = Object.keys(panel_data[cluster_name]).length;
+                        if (length > 360) {
+                            new_box_size += ' step1';
                         }
-
-                        if (node_data['note']) {
-                            node_status += ' note';
-                        }
-
-                        if (node_data['nagios']) {
-                            node_status += node_data['nagios'];
-                        }
-
-                        // Node
-                        finished_node += '<span id="' + node_name + '" class="node ' + node_status.toLowerCase() + '" title="' + node_info + '"></span>';
                     }
                 }
-                else {
-                    finished_node += '<span title="No managed systems">&nbsp;&#x2205;</span>';
+
+                finished_node += '<div class="node-panel grid-item' + new_box_size + '" id="' + panel_name + '">';
+                finished_node += '<h3 class="panel-name">' + panel_name + '</h3>';
+
+                for (cluster_name in panel_data) {
+                    cluster_data = panel_data[cluster_name];
+
+                    if (current_view === 'elasticsearch-routing') {
+                        finished_node += '<div class="rack">';
+                    } else {
+                        finished_node += '<div class="node-cluster">';
+                    }
+
+                    if (cluster_name !== '') {
+                        finished_node += '<h5 class="cluster-name">' + cluster_name + '</h5>';
+                    }
+
+                    if (cluster_data.length !== 0) {
+                        for (node_name in cluster_data) {
+                            node_data = cluster_data[node_name];
+                            node_info = '<h4>' + node_name + '</h4>';
+
+                            node_status = 'unknown';
+
+                            for (info in node_data) {
+                                info_body = '';
+
+                                if (info === 'status') {
+                                    info = '<h4>Status:</h4>';
+                                    node_status = "";
+                                    for (status in node_data['status']) {
+                                        node_status += status;
+                                    }
+                                    info_body += '<ul><b>' + node_status.toLowerCase() + '</b> - ' + node_data['status'][status] + '</ul>';
+                                } else {
+                                    info_body = node_data[info];
+                                    info = '<b>' + info.replace(/_/g, ' ') + '</b>: ';
+                                }
+                                node_info += '<p>' + info + info_body +'</p>';
+                            }
+
+                            if (node_data['note']) {
+                                node_status += ' note';
+                            }
+
+                            if (node_data['nagios']) {
+                                node_status += node_data['nagios'];
+                            }
+
+                            // Node
+                            finished_node += '<span id="' + node_name + '" class="node ' + node_status.toLowerCase() + '" title="' + node_info + '"></span>';
+                        }
+                    }
+                    else {
+                        finished_node += '<span title="No managed systems">&nbsp;&#x2205;</span>';
+                    }
+                    finished_node += '</div>';
                 }
                 finished_node += '</div>';
             }
             finished_node += '</div>';
         }
-        finished_node += '</div>';
     }
 
     $('#farm').empty();
@@ -155,7 +165,7 @@ function gotData(data) {
     });
 
     // Node window
-    if (current_view !== 'elasticsearch-routing' && current_view !== 'elasticsearch-hosts') {
+    if (clickable !== false) {
         $('.node').click(function() {
             open_node(this.id);
         });
