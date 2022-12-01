7273d91c11b1356a786a2e50acc38d2faca3e7a5
James Adams <james.adams@stfc.ac.uk>
2022-12-01 16:22:57 +0000

    view-logical-workers: Use aquilon as source-of-truth
    Stop using MagDB entirely.

--- views/view-logical-workers.php
+++ views/view-logical-workers.php
@@ -1,20 +1,26 @@
 <?php
 require("header.php"); // Important includes
-require("inc/db-magdb-open.inc.php"); // Postgres Data Sources
 
 // Configuration
+$AQUILON_URL = $CONFIG['URL']['AQUILON'];
 $config = Array(
     "clickable" => true,
 );
 
-// Gathers clusters
-$all_clusters = Array();
-$data = pg_query('select "name", "categoryName" from "vBatchAndCloudVMs"');
-if ($data and pg_num_rows($data)){
-    while ($row = pg_fetch_assoc($data)) {
-        $all_clusters[$row['name']] = $row['categoryName'];
-    }
+function is_worker_node($var) {
+    return ((strpos($var, "wn-") !== false) || (strpos($var, "hadoop-") !== false));
 }
+
+// Get hosts and models from aquilon
+$url = "$AQUILON_URL/cgi-bin/report/report_host_model";
+$jsondata = file_get_contents($url);
+if ($jsondata === false) {
+    error("No data returned from", "aquilon");
+}
+$all_clusters = json_decode($jsondata, true);
+$all_clusters = array_filter($all_clusters, "is_worker_node");
+uksort($all_clusters, "strnatcmp");
+
 
 // Gathers notes
 $all_notes = Array();
@@ -27,7 +33,7 @@ if ($notes and $notes->num_rows) {
 
 // Gathers states
 $all_status = Array();
-$status = $SQL->query("select name, state, source from state");
+$status = $SQL->query("select name, state, source from state where name not like '%.scarf.rl.ac.uk'");
 if ($status and $status->num_rows) {
     while ($state = $status->fetch_assoc()) {
         $all_status[$state['name']] = Array(
@@ -36,44 +42,42 @@ if ($status and $status->num_rows) {
     }
 }
 
-// Gathers VM node if a worker
-$personality = Array();
-
-$vms = get_aquilon_report("host_personality_branch_nubes");
-foreach ($vms as $name => $values) {
-    foreach ($values as $key => $value) {
-        if (strpos($value, "workernode") !== false) {
-            $personality[$name][$value] = $name;
-        }
-    }
-}
+$personalities = get_aquilon_report($CONFIG, 'host_personality_branch');
+$models = get_aquilon_report($CONFIG, 'report_host_model');
 
 // Generates main array
 $results = Array();
 $results['config'] = $config;
+
+
 foreach ($all_clusters as $name => $panels) {
-    if (($panels !== "vm-nubes") or (array_key_exists($name, $personality) === true)) {
+    if (($panels !== "vm-nubes") and (array_key_exists($name, $personalities) === true)) {
 
         $group = '';
         $panel = $panels;
         $cluster = '';
 
+        if (array_key_exists($name, $personalities)) {
+            $cluster = $personalities[$name]['personality'];
+        };
+
         $results[$group][$panel][$cluster][$name] = Array();
         if (array_key_exists($name, $all_notes)) {
             $results[$group][$panel][$cluster][$name]['note'] = $all_notes[$name];
-        }
+        };
         if (nagios($name) !== Null) {
             $results[$group][$panel][$cluster][$name]['nagios'] = nagios($name);
-        }
+        };
         if (array_key_exists($name, $all_status)) {
             $results[$group][$panel][$cluster][$name]['status'] = $all_status[$name];
             unset($all_status[$name]);
-        }
+        };
     }
 }
 
-// Render all remaining hosts from the batch state table
-// this catches any hosts from cloud-bursting or other dynamic sources
+
+// Render all leftover hosts from the batch state table
+// this catches any hosts created via cloud-bursting
 foreach ($all_status as $hostname => $status) {
     if ($hostname !== '') {
         $source = '';
@@ -81,12 +85,36 @@ foreach ($all_status as $hostname => $status) {
             $source = array_values($status);
             $source = $source[0];
         }
-        $results['']['dynamic'][$source][$hostname] = Array(
+
+        $model = $source;
+        if (array_key_exists($hostname, $models)) {
+            $model = $models[$hostname];
+        };
+
+        $personality = '∅';
+        if (array_key_exists($hostname, $personalities)) {
+            if (array_key_exists('personality', $personalities[$hostname])) {
+                $personality = $personalities[$hostname]['personality'];
+            };
+        };
+
+        $details = Array(
             'status' => $status,
         );
+
+        if (array_key_exists($hostname, $all_notes)) {
+            $details['note'] = $all_notes[$hostname];
+        };
+        if (nagios($hostname) !== Null) {
+            $details['nagios'] = nagios($hostname);
+        };
+
+        if ($source !== "Slurm on SCARF") {
+            $results[''][$model][$personality][$hostname] = $details;
+        };
     };
 };
 
 
 // Returns built json
-echo json_encode($results);
+echo json_encode($results) ;
